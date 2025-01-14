5108704c8ff476212b4a1a9b53e52f7bfe3a1702
Chathuni Ambawatta <152898986+CMalsha@users.noreply.github.com>
2025-01-14 13:35:33 +0530

    Update Register.test.ts

--- Register.test.ts
+++ Register.test.ts
@@ -7,7 +7,7 @@ test("Register test demo", async () => {
     const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto("https://ecommerce-playground.lambdatest.io/");
-    /*await page.hover('//*[@id="widget-navbar-217834"]/ul/li[6]/a/div/span')
+    await page.hover('//*[@id="widget-navbar-217834"]/ul/li[6]/a/div/span')
     await page.click("text =Register")
     await page.fill('input[name="firstname"]',"Priya");
     await page.fill('input[name="lastname"]',"Bole");
@@ -21,9 +21,7 @@ test("Register test demo", async () => {
     // Click the Terms & Conditions checkbox
     await page.click("text=agree");
     await page.click('//*[@id="content"]/form/div/div/input');
-    */
 
- 
     //navigate to the login page
     await page.hover('//*[@id="widget-navbar-217834"]/ul/li[6]/a/div/span');
     await page.click("text=Login");
@@ -35,22 +33,20 @@ test("Register test demo", async () => {
 
     await page.hover('//*[@id="entry_217832"]/a');
     await page.click('//*[@id="entry_217832"]/a');
-
     
-      // Hover over "Phone, Tablets & Ipod"
-      await page.hover("text= Phone, Tablets & Ipod");
-      
-      
-      // Click on "Mobile Phones"
-      await page.click("text= Phone, Tablets & Ipod");
+    // Hover over "Phone, Tablets & Ipod"
+    await page.hover("text= Phone, Tablets & Ipod");
+    
+    // Click on "Mobile Phones"
+    await page.click("text= Phone, Tablets & Ipod");
   
-      await page.click('//*[@id="mz-product-grid-image-28-212408"]/div/div[1]/img');
-      console.log("Product 'HTC Touch HD' selected");
-      await page.click('//*[@id="entry_216842"]/button');
-      console.log("clicked 'Add to Cart Button");
+    await page.click('//*[@id="mz-product-grid-image-28-212408"]/div/div[1]/img');
+    console.log("Product 'HTC Touch HD' selected");
+    await page.click('//*[@id="entry_216842"]/button');
+    console.log("clicked 'Add to Cart Button");
       
-      // Close the browser
-      await browser.close();
+    // Close the browser
+    await browser.close();
 
     
 
